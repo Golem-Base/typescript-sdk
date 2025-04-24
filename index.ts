bc987d5fdefd5b071fe9095601254879407f6ca9
r-vdp <ramses@well-founded.dev>
2025-04-24 18:09:26 +0200

    chore: make a bunch of fields readonly

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ export * as internal from "./src/internal/client"
  * Type representing an annotation with a key and a value, used for efficient lookups
  */
 export class Annotation<V> {
-  key: string
-  value: V
+  readonly key: string
+  readonly value: V
 
   constructor(key: string, value: V) {
     this.key = key
@@ -25,45 +25,45 @@ export type Hex = `0x${string}`
  * Type representing a create transaction in GolemBase
  */
 export type GolemBaseCreate = {
-  data: string,
-  ttl: number,
-  stringAnnotations: StringAnnotation[]
-  numericAnnotations: NumericAnnotation[],
+  readonly data: string,
+  readonly ttl: number,
+  readonly stringAnnotations: StringAnnotation[]
+  readonly numericAnnotations: NumericAnnotation[],
 }
 /**
  * Type representing an update transaction in GolemBase
  */
 export type GolemBaseUpdate = {
-  entityKey: Hex,
-  data: string,
-  ttl: number,
-  stringAnnotations: StringAnnotation[]
-  numericAnnotations: NumericAnnotation[],
+  readonly entityKey: Hex,
+  readonly data: string,
+  readonly ttl: number,
+  readonly stringAnnotations: StringAnnotation[]
+  readonly numericAnnotations: NumericAnnotation[],
 }
 /**
  * Type representing an extend transaction in GolemBase
  */
 export type GolemBaseExtend = {
-  entityKey: Hex,
-  numberOfBlocks: number,
+  readonly entityKey: Hex,
+  readonly numberOfBlocks: number,
 }
 /**
  * Type representing a delete transaction in GolemBase
  */
 export type GolemBaseTransaction = {
-  creates?: GolemBaseCreate[],
-  updates?: GolemBaseUpdate[],
-  deletes?: Hex[],
-  extensions?: GolemBaseExtend[],
+  readonly creates?: GolemBaseCreate[],
+  readonly updates?: GolemBaseUpdate[],
+  readonly deletes?: Hex[],
+  readonly extensions?: GolemBaseExtend[],
 }
 
 /**
  * Type representing the metadata of a entity stored in GolemBase
  */
 export type EntityMetaData = {
-  expiresAtBlock: bigint,
-  payload: string,
-  stringAnnotations: StringAnnotation[],
-  numericAnnotations: NumericAnnotation[],
-  owner: Hex,
+  readonly expiresAtBlock: bigint,
+  readonly payload: string,
+  readonly stringAnnotations: StringAnnotation[],
+  readonly numericAnnotations: NumericAnnotation[],
+  readonly owner: Hex,
 }
